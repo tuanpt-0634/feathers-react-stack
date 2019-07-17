6e9b033ff0895ed7752c51d024969018ba265c6f
Tuan Pham <pham.thanh.tuan@sun-asterisk.com>
2019-07-17 09:25:41 +0700

    Config log level

--- server/src/logger.js
+++ server/src/logger.js
@@ -1,9 +1,12 @@
 const { createLogger, format, transports } = require('winston');
+const configuration = require('@feathersjs/configuration');
+
+const config = configuration();
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
   // To see more detailed errors, change this to 'debug'
-  level: 'info',
+  level: config().debug ? 'debug' : 'info',
   format: format.combine(
     format.splat(),
     format.simple()
